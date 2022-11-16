201409886cb11fb91f01bf921ad56f3c4a3610dc
William <webuntermench@protonmail.com>
2022-11-16 19:06:55 +0300

    update

--- src/components/Offer.jsx
+++ src/components/Offer.jsx
@@ -6,16 +6,17 @@ export const Offer = () => {
             <div className={ofr.wrapper}>
                 <div className={ofr.sub}><span>Как мы</span></div>
                 <h3 className={ofr.main}>Работаем</h3>
-                <p className={ofr.descr}>Наша логистическая компания организует поставку, грузоперевозку и таможенную очистку грузов. Нам доверяют грузы различного назначения: компьютеры, серверы и комплектующие к ним, системы видеонаблюдения, комплектующие для телекоммуникационных систем связи, досмотровое рентгеновское оборудование, сельскохозяйственное оборудование и комплектующие к нему, хозяйственные товары бытового назначения, продовольственные товары и прочие грузы.</p>
+                <p className={ofr.descr}>
+                    Наша компания предоставляет комплекс сервисов по мониторингу соискателя внутри компании. 
+                </p>
                 <div className={ofr.container}>
                     <div className={ofr.element}>
                         <div className={ofr.element__step}>
                             <span>01</span>
                         </div>
                         <div className={ofr.element__block}>
-                            <h4 className={ofr.element__name}>Расчет сделки</h4>
-                            <p className={ofr.element__descr}>Клиент заполняет Заявку.
-    Наш Менеджер рассчитывает стоимость перевозки и сопутствующих услуг.</p>
+                            <h4 className={ofr.element__name}>Выбор плана</h4>
+                            <p className={ofr.element__descr}>Клиент выбирает интересующий План сервиса исходя из собственных материальных возможностей.</p>
                         </div>
                     </div>
                     <div className={ofr.element}>
@@ -23,9 +24,9 @@ export const Offer = () => {
                             <span>02</span>
                         </div>
                         <div className={ofr.element__block}>
-                            <h4 className={ofr.element__name}>Расчет сделки</h4>
+                            <h4 className={ofr.element__name}>Подписание договора</h4>
                             <p className={ofr.element__descr}>Клиент заполняет Заявку.
-    Наш Менеджер рассчитывает стоимость перевозки и сопутствующих услуг.</p>
+    Наш Менеджер рассчитывает стоимость сервиса и сопутствующих услуг.</p>
                         </div>
                     </div>
                     <div className={ofr.element}>
@@ -33,9 +34,8 @@ export const Offer = () => {
                             <span>03</span>
                         </div>
                         <div className={ofr.element__block}>
-                        <h4 className={ofr.element__name}>Расчет сделки</h4>
-                        <p className={ofr.element__descr}>Клиент заполняет Заявку.
-Наш Менеджер рассчитывает стоимость перевозки и сопутствующих услуг.</p>
+                        <h4 className={ofr.element__name}>Оплата сделки</h4>
+                        <p className={ofr.element__descr}>На основе заключенного договора заключается контракт и осуществляется перевод.</p>
                         </div>
                     </div>
                     <div className={ofr.element}>
@@ -43,9 +43,8 @@ export const Offer = () => {
                             <span>04</span>
                         </div>
                         <div className={ofr.element__block}>
-                        <h4 className={ofr.element__name}>Расчет сделки</h4>
-                        <p className={ofr.element__descr}>Клиент заполняет Заявку.
-Наш Менеджер рассчитывает стоимость перевозки и сопутствующих услуг.</p>
+                        <h4 className={ofr.element__name}>Сопровождение</h4>
+                        <p className={ofr.element__descr}>Наш Менеджер подготавливает и передает Клиенту полный комплект необходимых документов по выполненному контракту</p>
                         </div>
                     </div>
 
--- src/components/Sevices.jsx
+++ src/components/Sevices.jsx
@@ -1,31 +1,71 @@
 import srvc from "./Services.module.css"
-import image from "../assets/test.png"
+import image from "../assets/widget.png"
+import image1 from "../assets/trello.png"
+import { useState } from "react"
 
 export const Services = () => {
-    return(
+    const someData = [
+        {
+            title: "Расширение",
+            img: image,
+            li:[
+                "Первым делом необходимо ввести ваш секретный ключ в графе Api Key.",
+                "Парсинг осуществляется обводкой необходимой области.",
+                "При выделении ключевого слова появляется форма ввода, куда необходимо ввести заголовок критерия.",
+                "При нажатии клавиши действия или обводки другой области элемент сохраняется.",
+                "Открытие расширение осуществляется на нажатии кнопки около панели адресса.",
+                "При необходимости можно отредактировать уже готовые элементы или же удалить.",
+                "При составлении готового резюме кандидата необходимо нажать кнопку Push."
+            ]
+        },
+        {
+            title: "Клиент",
+            img: image1,
+            li:[
+                "Клиент представляет из себя доску",
+                "Столбцы представляют из себя стадии приема кандидата на итоговую работу",
+                "Все заного поступающие анкеты попадают в столбец Входящие вакансии",
+                "По ходу собеседования, рекрутер перемещает анкету по доске на соответствующий столбец",
+                "Можно расскрыть анкеты, для получения более информативного описания кандидата",
+                "Каждый элемент столбца может подвергаться редактированию (обновление, удаление)"
+            ]
+        }
+    ]
+    const [flag, setFlag] = useState(someData[0])
+    const [take, setTake] = useState(true)
+    
+    const changeTake = (event) => {
+        event.preventDefault()
+        if(take){
+            setFlag(someData[1])
+            setTake(false)
+            return
+        }
 
+        setFlag(someData[0])
+        setTake(true)
+    }
+
+    return(
         <section className={srvc.outer}>
             <div className={srvc.wrapper}>
-                <div className={srvc.sub}><span>Как мы</span></div>
-                <h3 className={srvc.main}>Работаем</h3>
-                <p className={srvc.descr}>Наша логистическая компания оказывает услуги ВЭД аутсорсинга и транспортной логистики. 
-    Наши менеджеры урегулируют все вопросы по ВЭД и организуют международную грузоперевозку любого товара, в том числе: любой сложности и любым видом транспорта, позволяя Вашей организации сконцентрироваться на ведении собственного  бизнеса.  
-    Мы возьмем на себя как организацию разовой поставки, так и обеспечение бесперебойного снабжения Вашего бизнеса товарами, сырьем,  оборудованием. Мы можем осуществить доставку товаров из-за рубежа «под ключ»: поиск поставщика, заключение контракта (как на себя, так и на Клиента), валютный перевод, сертификация, таможенная очистка, доставка “от двери до двери”.
+                <div className={srvc.sub}><span>Что мы</span></div>
+                <h3 className={srvc.main}>Предлагаем</h3>
+                <p className={srvc.descr}>Наша сервис предоставляет услуги по парсингу резюме кандидата по интересующим критериям (ключевым словам). Сервис не требует особых навыков со стороны рекрутера. Основная работа осуществляется внутри браузера: <span className={srvc.descr__point}>1)</span> разбор резюме на наличие интересующих ключевых слов <span className={srvc.descr__point}>2)</span> формирование из входящего потока элементов резюме кандидата <span className={srvc.descr__point}>3)</span> работа внутри сервиса по мониторингу состояния резюме кандидата (редактирования резюме, перемещение по внутренней доске, удалении).
     </p>
             </div>
             <div className={srvc.offer}>
                 <div className={srvc.offer__image_place}>
-                    <img src={image} alt="" />
+                    <img src={flag.img} alt="" />
                 </div>
                <div className={srvc.offer__text_place}>
-                    <h4 className={srvc.offer__title}>Контрактная логистика</h4>
+                    <h4 className={srvc.offer__title}>{flag.title}</h4>
                     <ul className={srvc.offer__nav}>
-                        <li>Lorem ipsum dolor sit amet.</li>
-                        <li>Lorem ipsum dolor sit amet consectetur.</li>
-                        <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci, vel.</li>
-                        <li>Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
+                        {flag.li.map((el, index) => {
+                            return(<li key={index}>{el}</li>)
+                        })}
                     </ul>
-                    <div className={srvc.next}><a href="#">Следущее</a></div>
+                    <div className={srvc.next}><a href="#" onClick={changeTake}>Следущее</a></div>
                </div>
             </div>
         </section>
